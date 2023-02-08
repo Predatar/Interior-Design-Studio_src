33218d99b2e36ebffc0d17fba31367073001ac6d
Maksym Martynenko <predatar24@gmail.com>
2023-02-08 14:50:52 +0200

    add swiper in about_us

---
+++ dist/js/script.about_us.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        spaceBetween: 12,
+        slidesPerView: 'auto',
+        grabCursor: true,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.slider-button-next',
+            prevEl: '.slider-button-prev'
+        },
+        scrollbar: {
+            el: '.slider-scrollbar'
+        }
+    });
+});
