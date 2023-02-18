8ed73c33a95f78ff4a1eb61733c915576a25c774
Maksym Martynenko <predatar24@gmail.com>
2023-02-18 19:55:46 +0200

    refactoring script.project.js

--- dist/js/script.project.js
+++ dist/js/script.project.js
@@ -1,30 +1,23 @@
-const ourProject = document.querySelector('.our-project__projects');
+window.addEventListener('DOMContentLoaded', () => {
+    const ourProject = document.querySelector('.our-project__projects');
+    const ourProjectGrid = new Isotope(ourProject, {
+        itemSelector: '.our-project__projects-item',
+        masonry: {
+            fitWidth: true,
+            gutter: 8
+        }
+    });
 
-const ourProjectGrid = new Isotope(ourProject, {
-    itemSelector: '.our-project__projects-item',
-    masonry: {
-        fitWidth: true,
-        gutter: 8
+    const filterBtn = document.querySelector('.heading__select');
+
+    function arrangeProjects(filter) {
+        ourProjectGrid.arrange({ filter });
     }
-});
 
-const filterBtn = document.querySelector('.heading__select');
-
-filterBtn.addEventListener('click', e => {
-    switch (e.target.dataset.filter) {
-        case 'House':
-            ourProjectGrid.arrange({ filter: '[data-filter = "House"]' });
-            break;
-        case 'Apartments':
-            ourProjectGrid.arrange({ filter: '[data-filter = "Apartments"]' });
-            break;
-        case 'Commerce':
-            ourProjectGrid.arrange({ filter: '[data-filter = "Commerce"]' });
-            break;
-        case 'Architecture':
-            ourProjectGrid.arrange({ filter: '[data-filter = "Architecture"]' });
-            break;
-        default:
-            break;
-    }
+    filterBtn.addEventListener('click', e => {
+        const filter = e.target.dataset.filter;
+        if (filter) {
+            arrangeProjects(`[data-filter="${filter}"]`);
+        }
+    });
 });
