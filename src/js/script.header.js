1e4c650690ce447c7403ee9301ceccb94a04e612
Maksym Martynenko <predatar24@gmail.com>
2023-02-18 15:33:00 +0200

    asd

--- src/js/script.header.js
+++ src/js/script.header.js
@@ -1,77 +1,44 @@
-const header = document.querySelector('.header');
-const isDarkTheme = header.classList.contains('header__dark');
-const headerLogo = document.querySelector('.header__logo a img');
-
 window.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.header');
+    const headerLogo = document.querySelector('.header__logo a img');
+    const isDarkTheme = header.classList.contains('header__dark');
     const hamburger = document.querySelector('.hamburger');
     const burger = document.querySelector('.burger');
     const sidemenuTitle = document.querySelector('.hamburger__title');
     const sidemenu = document.querySelector('.sidemenu');
     const headerBurger = document.querySelector('.header__action-burger');
 
-    hamburger.addEventListener('click', () => {
+    const toggleMenu = () => {
         burger.classList.toggle('burger__active');
         sidemenu.classList.toggle('show');
         sidemenuTitle.classList.toggle('hamburger__title__active');
         document.body.classList.toggle('overflow');
         headerBurger.classList.toggle('header__action-burger__active');
 
-        if(!header.classList.contains('header__fixed')) {
-            if (isDarkTheme) {
-                if (header.classList.contains('header__dark')) {
-                    header.classList.remove('header__dark');
-                    headerLogo.src = 'icons/Logo.svg';
-                } else {
-                    header.classList.add('header__dark');
-                    headerLogo.src = 'icons/Logo-dark.svg';
-                }
-            } else {
-                return;
-            }
-        }
-    });
-    headerBurger.addEventListener('click', () => {
-        headerBurger.classList.toggle('header__action-burger__active');
-        sidemenu.classList.toggle('show');
-        document.body.classList.toggle('overflow');
-        burger.classList.toggle('burger__active');
-        sidemenuTitle.classList.toggle('hamburger__title__active');
-
-        if(!header.classList.contains('header__fixed')) {
-            if (isDarkTheme) {
-                if (header.classList.contains('header__dark')) {
-                    header.classList.remove('header__dark');
-                    headerLogo.src = 'icons/Logo.svg';
-                } else {
-                    header.classList.add('header__dark');
-                    headerLogo.src = 'icons/Logo-dark.svg';
-                }
-            } else {
-                return;
-            }
-        }
-    });
-});
-
-window.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    if (window.scrollY >= 800) {
         if (!header.classList.contains('header__fixed')) {
+            if (isDarkTheme) {
+                header.classList.toggle('header__dark');
+                headerLogo.src = header.classList.contains('header__dark')
+                    ? 'icons/Logo-dark.svg'
+                    : 'icons/Logo.svg';
+            } else {
+                return;
+            }
+        }
+    };
+
+    hamburger.addEventListener('click', toggleMenu);
+    headerBurger.addEventListener('click', toggleMenu);
+
+    window.addEventListener('scroll', () => {
+        const shouldAddClass = window.scrollY >= 800;
+        if (shouldAddClass && !header.classList.contains('header__fixed')) {
             header.classList.add('header__fixed');
             if (!isDarkTheme) {
                 header.classList.add('header__dark');
                 headerLogo.src = 'icons/Logo-dark.svg';
             }
-        }
-        /* if(window.outerWidth >= 624){
-            document.body.style = 'margin-top: 96px';
-        } else {
-            document.body.style = 'margin-top: 58px';
-        } */
-
-    }
-    if (window.scrollY <= 800) {
-        if (header.classList.contains('header__fixed')) {
+        } else if (!shouldAddClass && header.classList.contains('header__fixed')) {
             header.classList.remove('header__fixed');
             document.body.style = 'margin-top: 0';
             if (!isDarkTheme) {
@@ -79,5 +46,5 @@ window.addEventListener('scroll', () => {
                 headerLogo.src = 'icons/Logo.svg';
             }
         }
-    }
+    });
 });
