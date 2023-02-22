ed6062cf7593cae0681c0a41260804c96147460c
Maksym Martynenko <predatar24@gmail.com>
2023-02-22 20:21:56 +0200

    fixed scrollbar and navigation arrows

--- dist/js/script.about_us.js
+++ dist/js/script.about_us.js
@@ -1,4 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const sliderNumberFirst = document.querySelector('.slider__number');
+    const btnNext = document.querySelector('.slider-button-next');
+    const btnPrev = document.querySelector('.slider-button-prev');
+    const scrollbarDrag = document.querySelector('.slider-scrollbar-drag');
+    const mas = [];
+    mas.push(0);
+
+    btnPrev.style.opacity = '0';
+    btnPrev.style.cursor = 'default';
+
     const swiper = new Swiper('.swiper', {
         spaceBetween: 12,
         slidesPerView: 'auto',
@@ -6,9 +16,41 @@ window.addEventListener('DOMContentLoaded', () => {
         navigation: {
             nextEl: '.slider-button-next',
             prevEl: '.slider-button-prev'
-        },
-        scrollbar: {
-            el: '.slider-scrollbar'
         }
+    });
+
+    let scrollDragWidth = 100 / swiper.slides.length;
+
+    function widthBar(size, length) {
+        const n = size;
+        for (i = 0; i < length; i++) {
+            mas.push(mas[i] + n);
+        }
+        mas.shift();
+    }
+
+    function arrowDefault () {
+        btnPrev.style.opacity = '1';
+        btnNext.style.opacity = '1';
+        btnPrev.style.cursor = 'pointer';
+        btnNext.style.cursor = 'pointer';
+    }
+
+    widthBar(scrollDragWidth, swiper.slides.length);
+
+    scrollbarDrag.style.width = `${mas[0]}%`;
+
+    swiper.on('slideChange', () => {
+        scrollbarDrag.style.width = `${mas[swiper.activeIndex]}%`;
+        if (swiper.activeIndex == 0) {
+            btnPrev.style.opacity = '0';
+            btnPrev.style.cursor = 'default';
+        } else if (swiper.activeIndex == swiper.slides.length - 1) {
+            btnNext.style.opacity = '0';
+            btnNext.style.cursor = 'default';
+        } else {
+            arrowDefault();
+        }
+        sliderNumberFirst.innerHTML = `0${swiper.activeIndex + 1}`;
     });
 });
--- dist/js/script.index.js
+++ dist/js/script.index.js
@@ -1,6 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
     const mainSelectSpan = document.querySelector('.main__select span');
     const mainOptionItem = document.querySelectorAll('.main__option-item');
+    const sliderNumberFirst = document.querySelector('.slider__number');
+    const btnNext = document.querySelector('.slider-button-next');
+    const btnPrev = document.querySelector('.slider-button-prev');
+    const scrollbarDrag = document.querySelector('.slider-scrollbar-drag');
+    const mas = [];
+    mas.push(0);
+
+    btnPrev.style.opacity = '0';
+    btnPrev.style.cursor = 'default';
 
     mainOptionItem.forEach(elem => {
         elem.addEventListener('click', e => {
@@ -19,11 +28,44 @@ window.addEventListener('DOMContentLoaded', () => {
             nextEl: '.slider-button-next',
             prevEl: '.slider-button-prev'
         },
-        scrollbar: {
-            el: '.slider-scrollbar'
-        },
         autoplay: {
             delay: 5000
         }
+    });
+
+    let scrollDragWidth = 100 / swiper.slides.length;
+
+    function widthBar(size, length) {
+        const n = size;
+        for (i = 0; i < length; i++) {
+            mas.push(mas[i] + n);
+        }
+        mas.shift();
+    }
+
+    function arrowDefault () {
+        btnPrev.style.opacity = '1';
+        btnNext.style.opacity = '1';
+        btnPrev.style.cursor = 'pointer';
+        btnNext.style.cursor = 'pointer';
+    }
+
+    widthBar(scrollDragWidth, swiper.slides.length);
+
+    scrollbarDrag.style.width = `${mas[0]}%`;
+
+    swiper.on('slideChange', () => {
+        scrollbarDrag.style.width = `${mas[swiper.activeIndex]}%`;
+        arrowDefault();
+        if (swiper.activeIndex == 0) {
+            btnPrev.style.opacity = '0';
+            btnPrev.style.cursor = 'default';
+        } else if (swiper.activeIndex == swiper.slides.length - 1) {
+            btnNext.style.opacity = '0';
+            btnNext.style.cursor = 'default';
+        } else {
+            arrowDefault();
+        }
+        sliderNumberFirst.innerHTML = `0${swiper.activeIndex + 1}`;
     });
 });
